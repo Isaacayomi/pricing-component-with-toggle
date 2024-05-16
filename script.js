f1b17ef83ce3d98060339efdffd95ed91aba54d4
Isaac Ayomide <okunlolaa3@outlook.com>
2024-05-16 06:05:10 +0100

    added key event

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
+const bodyEl = document.querySelector("body");
 const toggleEl = document.querySelector(".toggle__btn");
 const basicPrice = document.querySelector(".price__basic");
 const proPrice = document.querySelector(".price__pro");
 const masterPrice = document.querySelector(".price__master");
 
-toggleEl.addEventListener("click", function (e) {
+const toggleMonthlyPrice = () => {
+  toggleEl.classList.add("move__toggle");
+  basicPrice.textContent = 19.99;
+  proPrice.textContent = 24.99;
+  masterPrice.textContent = 39.99;
+};
+
+const toggleAnuualPrice = () => {
+  toggleEl.classList.remove("move__toggle");
+  basicPrice.textContent = 199.99;
+  proPrice.textContent = 249.99;
+  masterPrice.textContent = 399.99;
+};
+
+const toggleBtn = () => {
   if (!toggleEl.classList.contains("move__toggle")) {
-    toggleEl.classList.add("move__toggle");
-    basicPrice.textContent = 199.99;
-    proPrice.textContent = 249.99;
-    masterPrice.textContent = 399.99;
+    toggleMonthlyPrice();
   } else {
-    toggleEl.classList.remove("move__toggle");
-    basicPrice.textContent = 19.99;
-    proPrice.textContent = 24.99;
-    masterPrice.textContent = 39.99;
+    toggleAnuualPrice();
+  }
+};
+
+toggleEl.addEventListener("click", toggleBtn);
+bodyEl.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    toggleMonthlyPrice();
+  } else {
+    toggleAnuualPrice();
   }
 });
